16739a8df7dbd06853afcc46907c1b160c0f86fb
GZZZ <walter.gz@icloud.com>
2024-10-14 08:13:57 -0600

    Andamos readi

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,20 @@
 import lista.LinkedList;
 
+import java.sql.ClientInfoStatus;
 import java.util.Iterator;
+import java.util.Random;
 
 // Compendio de estructuras de datos de la materia Estructuras de datos
  // Lets f go
 public class Main {
     public static void main(String[] args) {
         LinkedList<Integer> list = new LinkedList<>();
-        list.add(11);
-        list.add(22);
-        list.add(33);
+
+
+        Random random = new Random();
+        for (int i = 0; i < 1000 ; i++){
+            list.add(random.nextInt(1,132043));
+        }
         Iterator iterator = list.iterator();
 
     }
